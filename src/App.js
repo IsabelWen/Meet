6731204c9bfd61a3511f54b32349e7a4ad06ab26
Isy <isy1997@freenet.de>
2024-01-01 15:20:35 +0100

    task 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
+      <NumberOfEvents />
+      <EventList />
     </div>
   );
 }
