6b126cba3315768e498ecb76ca71c2c2ecdb443c
Isy <isy1997@freenet.de>
2024-01-10 17:50:15 +0100

    Install Atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('9200d279c9484252bb090d5bb3520f37').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
